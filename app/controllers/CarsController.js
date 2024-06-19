6e556546c891695661546143ad1a9e3a6488afd1
Jeremy Fowler <jembyfowler@gmail.com>
2024-06-19 10:28:31 -0600

    drawing cars

--- app/controllers/CarsController.js
+++ app/controllers/CarsController.js
@@ -1,6 +1,16 @@
+import { AppState } from "../AppState.js";
+import { setHTML } from "../utils/Writer.js";
 
 export class CarsController {
   constructor() {
     console.log('Cars Controller is loaded!');
+    this.drawCars()
+  }
+
+  drawCars() {
+    const cars = AppState.cars
+    let innerHTMLString = ''
+    cars.forEach((car) => innerHTMLString += car.cardHTMLTemplate)
+    setHTML('carListings', innerHTMLString)
   }
 }
